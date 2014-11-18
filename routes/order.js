919d632352524cd8404e2fd6afa6c5bc1f7244e9
tom-chen <thomas.chen@students.olin.edu>
2014-11-17 22:35:14 -0500

    fixed price display :rice-ball:

--- routes/order.js
+++ routes/order.js
@@ -13,7 +13,9 @@ exports.confirm = function(req, res){
       console.log('Order saved successfully.')
     }
   })
-  var buttonmap = {"Walnut": "GE7FHCVVBACEE", "Cherry": "MEN438Z29J866", "Birch": "4SAPHAYVLUY8S", "Cardstock": "YBKPVF5GSJK6S"}
+  var buttonmap = {"Walnut": "GE7FHCVVBACEE", "Cherry": "MEN438Z29J866", "Birch": "4SAPHAYVLUY8S", "Cardstock": "YBKPVF5GSJK6S"};
+  var pricemap = {"Walnut": ["8.50", "17.00", "25.50", "34.00"], "Cherry": ["8.50", "17.00", "25.50", "34.00"], "Birch": ["7.00", "14.00", "21.00", "28.00"], "Cardstock": ["2.50", "5.00", "7.50", "10.00"]}
   var buttoncode = buttonmap[req.session.material];
-  res.render('order-confirm', {title: 'Confirm Your Order - LaserCards', material: req.session.material, name: req.session.name, image: req.session.image, email: req.session.email, button: buttoncode});
+  var baseprice = pricemap[req.session.material];
+  res.render('order-confirm', {title: 'Confirm Your Order - LaserCards', material: req.session.material, name: req.session.name, image: req.session.image, email: req.session.email, button: buttoncode, baseprice: baseprice});
 };
